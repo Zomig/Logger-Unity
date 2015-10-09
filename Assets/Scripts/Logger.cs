bf87f0e621e0b3575c94c8cfb8712672a84211a1
AndresMR <andres.miguelrubio@gmail.com>
2015-10-09 17:58:29 +0200

    Code upload

---
+++ Assets/Scripts/Logger.cs
@@ -0,0 +1,14 @@
+﻿namespace LoggerUnity
+{
+    using UnityEngine;
+
+    public class Logger
+    {
+        public static void Log(string message)
+        {
+            #if UNITY_EDITOR
+                Debug.Log(message);
+            #endif            
+        }
+    }
+}
---
+++ Assets/Scripts/PlayTest.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class PlayTest : MonoBehaviour {
+
+	void Start ()
+    {
+        Debug.Log("Probando Logger");
+
+        LoggerUnity.Logger.Log("Se muestra el log");
+	}	
+}
